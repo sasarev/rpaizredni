c738b3708f88cd4d168c9b768f1b943dc1b4cad1
srev <sasa.rev@scng.si>
2025-02-11 19:02:36 +0100

    mvc filmi

---
+++ MVCFilmi/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace MVCFilmi.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Pozdrav(string ime, int numTimes = 1)
+        {
+
+            ViewBag.sporocilo = $"Pozdravljen {ime}: kolikokrat {numTimes}";
+            ViewBag.numTimes = numTimes; 
+
+            return View();
+        }
+    }
+}
---
+++ MVCFilmi/Models/Film.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MVCFilmi.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public string Naslov { get; set; }
+        public DateTime Izdan { get; set; }
+        public string Tip { get; set; }
+        public decimal Cena { get; set; }
+    }
+}
